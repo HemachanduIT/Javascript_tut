a048e4420f8148b86c2c74f53bdbc95a628fc85a
chandu <hemachandubenuboyina@gmail.com>
2025-09-17 23:54:46 +0530

    feat:js for backend

--- JS BACKEND/api.js
+++ JS BACKEND/api.js
@@ -1,22 +1,45 @@
 // Asynchronous by defalut in NODE
 
-// function getJoke(){
-//     let data=fetch("https://v2.jokeapi.dev/joke/Dark",{
-//     method:"GET"
-// })
-// console.log(data);                      //Promise { <pending> }
-// console.log("chandu");                  //chandu
-// }
-// getJoke();
+function getJoke(){
+    let data=fetch("https://v2.jokeapi.dev/joke/Dark",{
+    method:"GET"
+})
+console.log(data);                     
+console.log("chandu");
+}
+getJoke();
+//output:
+// Promise { <pending> }
+// chandu
 
 // Converting asynchronous code into synchronous as follows:
 
-async function getJoke(){
-    let k=await fetch("https://v2.jokeapi.dev/joke/Dark",{
-        method:"GET"
-    })
-    let data=await k.json();
-    console.log(data);
-    console.log("chandu");
-    return
-}
+// async function getJoke(){
+//     let k=await fetch("https://v2.jokeapi.dev/joke/Dark",{
+//         method:"GET"
+//     })
+//     let data=await k.json();
+//     console.log(data);
+//     console.log("chandu");
+// }
+// getJoke();
+// //output:
+// {
+//   error: false,
+//   category: 'Dark',
+//   type: 'twopart',
+//   setup: 'What is the difference between acne and a catholic priest?',
+//   delivery: 'Acne usually comes on a boys face *after* he turns 12.',
+//   flags: {
+//     nsfw: true,
+//     religious: true,
+//     political: false,
+//     racist: false,
+//     sexist: false,
+//     explicit: true
+//   },
+//   id: 158,
+//   safe: false,
+//   lang: 'en'
+// }
+// chandu
